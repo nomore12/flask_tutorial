a60c19d86c0d3fae3ca50959db3bd1666a131566
nomore <nightwing@naver.com>
2021-01-25 21:34:56 +0900

    tutorial

--- project/app.py
+++ project/app.py
@@ -12,10 +12,26 @@ def index():
 def result(name=None):
     if request.method != "POST":
         return redirect('/')
-    name = request.form['name']
-    if name != '김치':
+    food = request.form['food']
+    if food != '김치':
         return redirect('/')
-    return render_template('result.html', name=name)
+    result = {'food': food, "food2": "dsdfasdfag"}
+    return render_template('result.html', **{"food": food, "food2": "복숭아"})
+
+
+lst = [ { "name": "노성호", "birth": "07/18" }, { "name": "강지능", "birth": "12/10" },{ "name": "임한국", "birth": "04/43" },{ "name": "이선주", "birth": "09/10" } ]
+
+@app.route('/list')
+def list():
+    return render_template('list.html', lst=lst)
+
+@app.route('/person/<int:id>')
+def person(id):
+    return render_template(
+        'detail.html', 
+        name=lst[id-1]['name'],
+        birth=lst[id-1]['birth']
+        )
 
 
 if __name__ == '__main__':
