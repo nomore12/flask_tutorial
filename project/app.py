6b8be205c646259a4c9b53c9b8c81b663c2be939
Sungho <nightwing@gmail.com>
2021-01-30 23:19:14 +0900

    기본적인 글쓰기 가능

--- project/app.py
+++ project/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
-from query import get_all_articles, get_all_users, get_user, get_article
+from query import get_all_articles, get_all_users, get_user, get_article, create_article
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -21,15 +23,22 @@ def index():
     return render_template('list.html', context=context)
 
 
-@app.route('/write')
-def create_article():
+# 유저는 무조건 1번. 하드코딩.
+@app.route('/edit')
+def write_article_page():
+    return render_template('edit.html', context=get_user(1))
+
+
+# 1번 유저만 글 쓸 수 있음.
+@app.route('/create', methods=['POST', ])
+def write_article():
     if request.method != 'POST':
-        return redirect('/write')
-
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-
-    pass
+        return redirect(url_for('.create'))
+    user_id = request.form['user_id']
+    title = request.form['title']
+    content = request.form['content']
+    create_article(user_id, title, content)
+    return redirect(url_for('.index'))
 
 
 if __name__ == '__main__':
